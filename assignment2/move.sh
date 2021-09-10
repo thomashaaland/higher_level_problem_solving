34a93e9c2241bdf3ae88e02739956fee86bd7595
Thomas Haaland <thomhaa@ifi.uio.no>
2021-09-10 22:58:53 +0200

    Can choose to create directory when moving files

--- assignment2/move.sh
+++ assignment2/move.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+function mover {
+    echo "Starting file transfer."
+    
+    if [ -z $ftype ]
+    then
+	files=$(ls $src)
+    else
+	files=$(ls $src | grep ".$ftype")
+    fi
+    # Moving files here
+    echo "Found ${#files[@]} file(s)"
+    for file in $files
+    do
+	echo "Transfering file ${file}..."
+	mv "${src}/${file}" ${dst}
+    done
+    echo "Done!"
+}
+
 # Check that we get at least 2 arguments from terminal
 if [ "$#" -ge "2" ]
 then # If we do capture the three first arguments in src, dst and ftype
@@ -21,26 +40,20 @@ then
     exit 999 # die with error code 999
 # Check that destination directory exists
 elif [ ! -d "$dst" ]; then
-    echo "ERROR: Directory $dst does not exist! Please create the directory before trying again."
-    exit 999 # die with error code 999
+    echo "The directory $dst does not exist. Do you want to create the directory? [yes/no]"
+    read answer
+    case "$answer" in
+	"Yes" | "yes" | "y" | "Y")
+	    echo "Creating directory $dst"
+	    mkdir $dst
+	    mover;;
+	"No" | "no" | "n" | "N" | *)
+	    echo "ERROR: Directory $dst does not exist! Please create the directory before trying again."
+	    exit 999 # die with error code 999
+    esac
 # All is good so far, lets start moving files
 else
     echo "Both directories exist."
-    echo "Starting file transfer."
     # Check to see if the user wants to move files of a particular type
-    if [ -z $ftype ]
-    then
-	files=$(ls $src)
-    else
-	files=$(ls $src | grep ".$ftype")
-    fi
-    # Moving files here
-    echo "Found ${#files[@]} file(s)"
-    for file in $files
-    do
-	echo "Transfering file ${file}..."
-	mv "${src}/${file}" ${dst}
-    done
-    echo "Done!"
+    mover
 fi
-
