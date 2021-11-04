d37c0a560e60210cea71786abdbc21ad4c241146
Thomas Haaland <thomhaa@ifi.uio.no>
2021-11-04 10:18:59 +0100

    Start ignoring some files

--- assignment4/task-4.1/setup.py
+++ assignment4/task-4.1/setup.py
@@ -2,6 +2,9 @@ import numpy
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(name='Cython modules',
-      ext_modules=cythonize("*.pyx"),
-      include_dirs=[numpy.get_include()])
+setup(
+    name='Cython modules',
+    ext_modules=cythonize("*.pyx", annotate = True),
+    include_dirs=[numpy.get_include()]
+)
+
