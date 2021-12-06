df1b111a1c86f2097fb69d50d4219dc1e2fb3673
Thomas Haaland <thomhaa@ifi.uio.no>
2021-12-06 15:05:14 +0100

    Interactive plot

--- assignment6/climate/climate_status_plots.py
+++ assignment6/climate/climate_status_plots.py
@@ -7,6 +7,8 @@ import altair as alt
 def get_data_from_csv():
     """Reads data from selected csv
     
+    Returns:
+       A pandas dataframe
     """
     filepath = "./sample_data/"
     filename = filepath + "climate_data_combined.csv"
@@ -15,37 +17,11 @@ def get_data_from_csv():
     df = df.drop(["Unnamed: 0"], axis=1)
     return df
     
-def plot_climate_status():
-    """Plots the data
-
-    Returns:
-       json (Altair chart): A json file which can be used to plot
-
-    """
-    num2months = {1: "January", 2: "February", 3: "March", 4: "April",
-              5: "May", 6: "June", 7: "July", 8: "August",
-              9: "September", 10: "October", 11: "November", 12: "December"}
-    months2num = {num2months[key]: key for key in num2months}
-    
-    climate_df = get_data_from_csv()
-
-    column_to_plot = "Total"
-    color = "Year"
-    
-    title = "Hello"
-
-    print(climate_df)
-    
-    chart = alt.Chart(climate_df, title=title).mark_line(size=1).encode(
-        x=alt.X("month:N", sort=[num2months[i] for i in range(1,13)]),
-        y=alt.Y("Total:Q"),
-        color=alt.Color("Year:Q"),
-    )
-
-    return chart.to_dict()
 
 def plot_climate_status():
-    """Plots the data
+    """Plots climate data. Yields an interactive plot
+    displaying the hottest year with its temperature
+    and coldest year with its temperature.
 
     Returns:
        json (Altair chart): A json file which can be used to plot
@@ -57,86 +33,77 @@ def plot_climate_status():
     months2num = {num2months[key]: key for key in num2months}
     monthSorted = [num2months[i] for i in range(1,13)]
     
-    climate_df = get_data_from_csv()
-
-    # Set title
-    title = "Hello"
-
-    # Format the table into seperate years
-    climate_df = climate_df.pivot(index="month", columns="Year", values="Total")
-    #climate_df.Year = climate_df.Year.astyp(str)
-    columns = climate_df.columns
-    climate_df["month"] = climate_df.index
-    climate_df.index = climate_df.month.apply(lambda x: months2num[x] - 1)
-    climate_df = (climate_df.sort_index())
-
-    # Fetch the last year
-    df = pd.DataFrame()
-    df["Temperature"] = climate_df[columns[-1]]
-    df["month"] = climate_df.month
-    #print(df)
-
-    highlight = alt.selection(
-        type="single", on="mouseover")#, fields=["Temperature"])
-    
-    base_chart = alt.Chart(df).encode(
-        x=alt.X("month", sort=monthSorted),
-        y=alt.Y("Temperature"))
-
-    points = base_chart.mark_circle().encode(
-        opacity=alt.value(0),
-        tooltip=[
-            alt.Tooltip("month", title="Month"),
-            alt.Tooltip("Temperature", title="Temperature")]
-    ).add_selection(highlight)
-    
-    lines = base_chart.mark_line().encode(
-        size=alt.condition(~highlight, alt.value(1), alt.value(3)))
-
-    acc_chart = (points + lines).interactive()
-    
-    for year in columns[:-1]:
-        df = pd.DataFrame()
-        df["Temperature"] = climate_df[year]
-        df["month"] = climate_df.month
-
-        base_chart = alt.Chart(df).encode(
-            x=alt.X("month", sort=monthSorted),
-            y=alt.Y("Temperature"))
-                
-        lines = base_chart.mark_line().encode(
-            opacity=alt.value(0.05))
-
-        acc_chart += lines
+    climate_df = get_data_from_csv()    
         
-    return acc_chart.properties(width=800, height=300).to_dict()
+    # Grab coldest and hottest years
+    hot = climate_df.groupby("month").max("Total")
+    cold = climate_df.groupby("month").min("Total")
+    hot_cold = hot.append(cold)
 
-    """
-    print(climate_df)
-
-    highlight = alt.selection(
-        type='single', on='mouseover', fields=["Year"], nearest=True)
+    hot_cold = hot_cold.reset_index()
+    print(hot_cold)
     
+    # Make the 'background' lines
     base_chart = alt.Chart(climate_df).encode(
         x=alt.X("month:N", sort=monthSorted,
                 scale=alt.Scale(domain=monthSorted, clamp=True)),
-        y=alt.Y("Value:Q", scale=alt.Scale(domain=[-2, 2])),
-        color=alt.Color("Year:O", scale=alt.Scale(scheme="greenblue")))
+        y=alt.Y("Total:Q", scale=alt.Scale(domain=[2, 16])),
+        color=alt.Color("Year:O", scale=alt.Scale(scheme="greenblue"), legend=None)
+    )
+    lines = base_chart.mark_line(opacity=0.2).encode()
 
-    points = base_chart.mark_circle().encode(
-        opacity=alt.value(0),
-        tooltip=[
-            alt.Tooltip("Year:O", title="Year"),
-            alt.Tooltip("month:N", title="Month"),
-            alt.Tooltip("Temperature:Q", title="Temperature")
-            ]).add_selection(highlight)
+    # Create the max and min plots. This plot is the interactive one
+    label = alt.selection_single(
+        encodings=['x'],
+        on="mouseover",
+        nearest=True,
+        empty="none"
+    )
 
-    lines = base_chart.mark_line().encode(
-        size=alt.condition(~highlight, alt.value(1), alt.value(3)))
+    base = alt.Chart(hot_cold).mark_line().encode(
+        alt.X('month:N', sort=monthSorted),
+        alt.Y('Total:Q'),
+        alt.Color("Year:N")
+    )
 
-    chart = (points + lines).properties(width=800, height=300).interactive()
-    """
-    #return chart.to_dict()
+    layer = alt.layer(
+        base,
+
+        # Rule chart
+        alt.Chart().mark_rule(color='#aaa').encode(
+            x=alt.X('month:N', sort=monthSorted),
+            ).transform_filter(label),
+
+        # add circle marks for selected month points, hide unselected points
+        base.mark_circle().encode(
+            opacity=alt.condition(label, alt.value(1), alt.value(0))
+        ).add_selection(label),
+
+        # Add white stroked text to provide a legible background for labels
+        base.mark_text(align='left', dx=5, dy=-5, stroke="white", strokeWidth=2).encode(
+            text='Total:Q'
+        ).transform_filter(label),
+
+        # add text labels for temperature
+        base.mark_text(align='left', dx=5, dy=-5, stroke="black").encode(
+            text="Total:Q"
+        ).transform_filter(label),
+
+                # Add white stroked text to provide a legible background for labels
+        base.mark_text(align='left', dx=5, dy=-20, stroke="white", strokeWidth=2).encode(
+            text='Year:Q'
+        ).transform_filter(label),
+
+        # add text labels for temperature
+        base.mark_text(align='left', dx=5, dy=-20, stroke="black").encode(
+            text="Year:Q"
+        ).transform_filter(label),
+
+        data = hot_cold
+    )
+        
+    chart = (lines + layer).properties(width=600, height=300)#.interactive()
+    return chart.to_dict()
     
 def main():
     plot_climate_status()
